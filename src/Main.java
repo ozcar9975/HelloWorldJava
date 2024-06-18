f6d6d440ba0ed32946d73fa5bf2f7aa35ac5b4b3
oscar <oscar.zhang9812@gmail.com>
2024-06-18 01:39:35 -0400

    first commit?

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.text.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+
+}
---
+++ src/OOP/oop_main.java
@@ -0,0 +1,20 @@
+package OOP;
+
+public class oop_main {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+        User u = new User();
+        u.name = "Caleb";
+        u.membership = "Gold";
+
+        User u2 = new User();
+        u2.name = "Sally";
+        u2.membership = "Silver";
+
+
+    }
+
+}
